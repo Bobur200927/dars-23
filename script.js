34d69a7543a83fff8f8ba1e7f15ecc5bc2b43959
Qarshiboyev Bobur <you@example.com>
2024-01-25 14:02:52 +0500

    fxwedxdxe

---
+++ script.js
@@ -0,0 +1,65 @@
+let a = 15
+const b = 2024
+// b = 60
+
+// String
+let str = 'Salom \n O\'zbekiston '
+let str2 = "Dunyo" + str
+let str3 = `
+    ${str}
+`
+// console.log(str3);
+// console.log(str + str2);
+// Number
+let num = 15 // INTEGER
+let num2 = 15.12 // FLOAT DOUBLE
+let num3 = 15e12 // BIGINT 
+// Boolean
+
+let bool = true // false
+
+// PRIMITIV
+let u = undefined
+let n = null
+let nn = NaN
+// console.log("salom "  * 15);
+
+// ARRAY MASSIV TO'PLAM
+
+let arr = ["STR", 15, true, [1 , 2, 3,], undefined]
+// console.log(arr[3][1]);
+// console.log(arr.length);
+
+// OBJECT
+
+let obj = {
+    firstname: "Bobur",
+    lastname: 'Xamidov',
+    age: 34,
+    12: 'Number',
+    "Salom hammaga": '5156165156' 
+}
+let l = 'lastname'
+// console.log(obj.firstname);
+// console.log(obj["age"]);
+
+// console.log(obj[l]);
+// console.log(obj);
+// console.log(obj["firstname"]);
+// console.log(window);
+// console.log('Assalom')
+
+// String()
+// Number()
+Boolean()
+let m = ' '
+
+// 0, false, undefined, null, ''
+
+let bl = Boolean(m)
+// console.log(bl);
+// console.log(typeof(bl));
+
+// window.addEventListener('keydown',(e)=>{
+//     console.log(e);
+// })
